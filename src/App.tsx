53613b7072d94a73b0d8f56e93112596eab4bea3
smallriver0316 <stream0316@gmail.com>
2024-05-06 19:08:45 +0900

    Succeeded to render map

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { useEffect } from 'react';
+import { createMap } from 'maplibre-gl-js-amplify';
+import 'maplibre-gl/dist/maplibre-gl.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  useEffect(() => {
+    createMap({
+      container: 'map',
+      center: [139.7674681227469, 35.68111419325676],
+      zoom: 14,
+    });
+  });
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  return <div id="map" style={{ height: '100vh' }} />;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
 import { Amplify } from 'aws-amplify';
 import awsconfig from './aws-exports.ts';
-import App from './App.tsx';
-import './index.css';
 
 Amplify.configure(awsconfig);
 
