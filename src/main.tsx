3a5458f494be9f7ac93c8a532d6355e0070d4121
smallriver0316 <stream0316@gmail.com>
2024-05-06 10:29:18 +0900

    Introduced eslint and prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Amplify } from 'aws-amplify';
+import awsconfig from './aws-exports.ts';
+import App from './App.tsx';
+import './index.css';
+
+Amplify.configure(awsconfig);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
