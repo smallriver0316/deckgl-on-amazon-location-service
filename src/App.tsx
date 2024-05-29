e5e0e8ca3491f7ba17bb54166085e0bf126f0280
smallriver0316 <stream0316@gmail.com>
2024-05-30 06:50:10 +0900

    Succeeded to render Plateau 3D models with deck.gl

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react';
-import { DeckGL, ScatterplotLayer, MapViewState } from 'deck.gl';
+import { DeckGL, Tile3DLayer, MapViewState } from 'deck.gl';
 import { MapView } from '@aws-amplify/ui-react-geo';
+import { Tiles3DLoader } from '@loaders.gl/3d-tiles';
+import type { Tileset3D, Tile3D } from '@loaders.gl/tiles';
+import { Vector3 } from 'math.gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 
 const INITIAL_VIEW_STATE: MapViewState = {
-  longitude: 0.45,
-  latitude: 51.47,
+  longitude: 139.7674681227469,
+  latitude: 35.68111419325676,
   zoom: 11,
   bearing: 0,
   pitch: 0,
@@ -13,13 +16,26 @@ const INITIAL_VIEW_STATE: MapViewState = {
 
 function App() {
   const [viewport, setViewport] = useState<MapViewState>(INITIAL_VIEW_STATE);
+
   const layers = [
-    new ScatterplotLayer({
-      id: 'deckgl-circle',
-      data: [{ position: [0.45, 51.47] }],
-      getPosition: (d) => d.position,
-      getFillColor: [255, 0, 0, 100],
-      getRadius: 1000,
+    new Tile3DLayer({
+      id: 'tile-3d-layer',
+      pointSize: 1,
+      data: 'https://assets.cms.plateau.reearth.io/assets/aa/ecf312-95c2-4e24-8351-642f27e447b6/13100_tokyo23-ku_2022_3dtiles_1_1_op_bldg_13101_chiyoda-ku_lod1/tileset.json',
+      loader: Tiles3DLoader,
+      onTilesetLoad: (tile: Tileset3D) => {
+        const { cartographicCenter } = tile;
+        if (cartographicCenter) {
+          console.log(cartographicCenter);
+        }
+      },
+      onTileLoad: (tileHeader: Tile3D) => {
+        tileHeader.content.cartographicOrigin = new Vector3(
+          tileHeader.content.cartographicOrigin.x,
+          tileHeader.content.cartographicOrigin.y,
+          tileHeader.content.cartographicOrigin.z - 40
+        );
+      },
     }),
   ];
 
@@ -28,8 +44,8 @@ function App() {
       initialViewState={INITIAL_VIEW_STATE}
       controller
       layers={layers}
-      onViewStateChange={({ viewState }) =>
-        setViewport(viewState as MapViewState)
+      onViewStateChange={({ viewState }: { viewState: MapViewState }) =>
+        setViewport(viewState)
       }
     >
       <MapView
