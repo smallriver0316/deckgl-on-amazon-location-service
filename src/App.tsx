c68591119baff46dc970b7be7c6fa77e6cdad2eb
smallriver0316 <stream0316@gmail.com>
2024-05-31 06:25:07 +0900

    Added 3D models of Minato-ku

--- src/App.tsx
+++ src/App.tsx
@@ -19,9 +19,47 @@ function App() {
 
   const layers = [
     new Tile3DLayer({
-      id: 'tile-3d-layer',
+      id: 'tile-3d-layer-chiyoda-ku',
       pointSize: 1,
-      data: 'https://assets.cms.plateau.reearth.io/assets/aa/ecf312-95c2-4e24-8351-642f27e447b6/13100_tokyo23-ku_2022_3dtiles_1_1_op_bldg_13101_chiyoda-ku_lod1/tileset.json',
+      data: 'https://assets.cms.plateau.reearth.io/assets/97/0b3db1-d1d5-441a-8aa0-6d6d63361e20/13100_tokyo23-ku_2022_3dtiles_1_1_op_bldg_13101_chiyoda-ku_lod2/tileset.json',
+      loader: Tiles3DLoader,
+      onTilesetLoad: (tile: Tileset3D) => {
+        const { cartographicCenter } = tile;
+        if (cartographicCenter) {
+          console.log(cartographicCenter);
+        }
+      },
+      onTileLoad: (tileHeader: Tile3D) => {
+        tileHeader.content.cartographicOrigin = new Vector3(
+          tileHeader.content.cartographicOrigin.x,
+          tileHeader.content.cartographicOrigin.y,
+          tileHeader.content.cartographicOrigin.z - 40
+        );
+      },
+    }),
+    // new Tile3DLayer({
+    //   id: 'tile-3d-layer-chuo-ku',
+    //   pointSize: 1,
+    //   data: 'https://assets.cms.plateau.reearth.io/assets/38/9cf378-c397-49bb-a4fb-894ce86647d8/13100_tokyo23-ku_2022_3dtiles_1_1_op_bldg_13102_chuo-ku_lod2/tileset.json',
+    //   loader: Tiles3DLoader,
+    //   onTilesetLoad: (tile: Tileset3D) => {
+    //     const { cartographicCenter } = tile;
+    //     if (cartographicCenter) {
+    //       console.log(cartographicCenter);
+    //     }
+    //   },
+    //   onTileLoad: (tileHeader: Tile3D) => {
+    //     tileHeader.content.cartographicOrigin = new Vector3(
+    //       tileHeader.content.cartographicOrigin.x,
+    //       tileHeader.content.cartographicOrigin.y,
+    //       tileHeader.content.cartographicOrigin.z - 40
+    //     );
+    //   },
+    // }),
+    new Tile3DLayer({
+      id: 'tile-3d-layer-minato-ku',
+      pointSize: 1,
+      data: 'https://assets.cms.plateau.reearth.io/assets/83/fc7c3b-0044-4b20-8e12-9771d3a2821e/13100_tokyo23-ku_2022_3dtiles_1_1_op_bldg_13103_minato-ku_lod2/tileset.json',
       loader: Tiles3DLoader,
       onTilesetLoad: (tile: Tileset3D) => {
         const { cartographicCenter } = tile;
